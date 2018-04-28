1832f0170e02f8da750a724fc7485740bd776101
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-28 11:35:57 -0400

    add switch statement to bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -26,5 +26,45 @@ let connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     //Run the start function to begin program
-    //purchaseChoice();
+    managerChoices();
 });
+
+function managerChoices() {
+    inquirer
+        .prompt ([
+            {
+                name: "managerChoice",
+                type: "rawlist",
+                message: "What would you like to do?",
+                choices: [
+                    "View Products For Sale",
+                    "View Low Inventory",
+                    "Add To Inventory",
+                    "Add New Product"
+                ]
+            }
+        ]).then(function(answer) {
+            //switch statement for each response
+            switch (answer) {
+                case "View Products For Sale":
+                    display();
+                    break;
+                case "View Low Inventory":
+                    lowInventory();
+                    break;
+                case "Add To Inventory":
+                    addInventory();
+                    break;
+                case "Add New Product":
+                    newProduct();
+                    break;
+                default:
+                    console.log("Switch Statment Error at managerChoices()");
+                    break;
+            }
+        })
+}
+
+function display() {
+    tableDisplay
+}
